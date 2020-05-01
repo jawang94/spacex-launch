23617906aa826c43bab5ae6fa435b68a58c16e07
Jason Wang <wang.jason.010@gmail.com>
2020-05-01 15:07:33 -0700

    Implementing GraphQL Server

---
+++ development/server/src/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+const resolvers = require('./resolvers');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}!`);
+});
